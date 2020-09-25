0294c2e784d41600891c18b11398ae2c92dc9b75
dnov09 <dngige9@gmail.com>
2020-09-24 20:28:03 -0500

    finished lab
    - fixed 800 -> 8000 typo
    - boot into grub and asm & c system
    - added funky colors :)

--- kernel.c
+++ kernel.c
@@ -1,11 +1,13 @@
-#define ADDRESS 0xB800;
+#define ADDRESS 0xB8000;
 
 void main(void)
 {
     char *addr = (char *)ADDRESS;
     addr[0] = 'O';
-    addr[1] = 'K';
+    addr[1] = 0xA;
+    addr[2] = 'K';
+    addr[3] = 0xD;
 
-    for(;;)
+    for(;;);
     return;
 }
